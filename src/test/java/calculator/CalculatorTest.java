3ea5d82052204dd909224007a373f4c381fd4460
Anton_Leshchynskyi <Anton_Leshchynskyi@epam.com>
2017-11-20 17:35:02 +0200

    Improved Calculator testing

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -7,12 +7,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CalculatorTest {
-
+    private static final ComplexObject OBJECT_A = new ComplexObject(1, 2);
+    private static final ComplexObject OBJECT_B = new ComplexObject(3, 4);
+    private static final ComplexObject EXPECTED_RESULT = new ComplexObject(0, 0);
+    
     @Mock
     private CountingService countingService;
 
@@ -20,21 +25,43 @@ public class CalculatorTest {
     private Calculator calculator = new Calculator();
 
     @Test
-    public void addTest() {
-        // Given
-        ComplexObject objectA = new ComplexObject(1, 2);
-        ComplexObject objectB = new ComplexObject(3, 4);
+    public void addsByCountingService() {
+        given(countingService.add(OBJECT_A, OBJECT_B)).willReturn(EXPECTED_RESULT);
 
-        ComplexObject expectedResult = new ComplexObject(0, 0);
+        ComplexObject result = calculator.add(OBJECT_A, OBJECT_B);
 
-        Mockito.when(countingService.add(objectA, objectB)).thenReturn(expectedResult);
-        // When
+        then(countingService).should().add(OBJECT_A, OBJECT_B);
+        Assert.assertEquals(EXPECTED_RESULT, result);
+    }
 
-        ComplexObject result = calculator.add(objectA, objectB);
+    @Test
+    public void subtractsByCountingService() {
+        given(countingService.subtract(OBJECT_A, OBJECT_B)).willReturn(EXPECTED_RESULT);
 
-        // Then
-        Assert.assertEquals(expectedResult, result);
-        Mockito.verify(countingService).add(objectA, objectB);
+        ComplexObject result = calculator.subtract(OBJECT_A, OBJECT_B);
+
+        then(countingService).should().subtract(OBJECT_A, OBJECT_B);
+        Assert.assertEquals(EXPECTED_RESULT, result);
+    }
+
+    @Test
+    public void multipliesByCountingService() {
+        given(countingService.multiply(OBJECT_A, OBJECT_B)).willReturn(EXPECTED_RESULT);
+
+        ComplexObject result = calculator.multiply(OBJECT_A, OBJECT_B);
+
+        then(countingService).should().multiply(OBJECT_A, OBJECT_B);
+        Assert.assertEquals(EXPECTED_RESULT, result);
+    }
+
+    @Test
+    public void dividesByCountingService() {
+        given(countingService.divide(OBJECT_A, OBJECT_B)).willReturn(EXPECTED_RESULT);
+
+        ComplexObject result = calculator.divide(OBJECT_A, OBJECT_B);
+
+        then(countingService).should().divide(OBJECT_A, OBJECT_B);
+        Assert.assertEquals(EXPECTED_RESULT, result);
     }
 
 }
