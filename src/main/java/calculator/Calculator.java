84ba93b3e06924528785f0688baa76ee8f9a0f07
Anton_Leshchynskyi <Anton_Leshchynskyi@epam.com>
2017-11-20 18:14:14 +0200

    Improved statistics updating by calculator

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,26 +1,45 @@
 package calculator;
 
+import calculator.domain.ArithmeticOperation;
 import calculator.domain.ComplexObject;
 import calculator.service.CountingService;
+import calculator.service.StatisticsService;
 
 public class Calculator {
 
     private CountingService countingService;
+    private StatisticsService statisticsService;
 
     public void setCountingService(CountingService countingService) {
         this.countingService = countingService;
     }
 
+    public void setStatisticsService(StatisticsService statisticsService) {
+        this.statisticsService = statisticsService;
+    }
+
     public ComplexObject add(ComplexObject param1, ComplexObject param2) {
+        peek(ArithmeticOperation.ADD);
         return countingService.add(param1, param2);
     }
+
     public ComplexObject subtract(ComplexObject param1, ComplexObject param2) {
+        peek(ArithmeticOperation.SUBTRACT);
         return countingService.subtract(param1, param2);
     }
+
     public ComplexObject multiply(ComplexObject param1, ComplexObject param2) {
+        peek(ArithmeticOperation.MULTIPLY);
         return countingService.multiply(param1, param2);
     }
+
     public ComplexObject divide(ComplexObject param1, ComplexObject param2) {
+        peek(ArithmeticOperation.DIVIDE);
         return countingService.divide(param1, param2);
+    }
+    private void peek(ArithmeticOperation operation) {
+        if (statisticsService != null){
+            statisticsService.peek(operation);
+        }
     }
 }
---
+++ src/test/java/calculator/impl/CalculatorAspectTest.java
@@ -0,0 +1,64 @@
+package calculator.impl;
+
+import calculator.Calculator;
+import calculator.domain.ArithmeticOperation;
+import calculator.domain.ComplexObject;
+import calculator.service.CountingService;
+import calculator.service.StatisticsService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Answers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.BDDMockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CalculatorAspectTest {
+
+    private static final ComplexObject OBJECT_A = new ComplexObject(4, 2);
+    private static final ComplexObject OBJECT_B = new ComplexObject(2, 5);
+
+    @Mock
+    private CountingService countingService;
+
+    @Mock
+    private StatisticsService statistics;
+
+    @InjectMocks
+    private Calculator calculator;
+
+    @Test
+    public void peeksStatisticsOnAdding() throws Exception {
+        calculator.add(OBJECT_A, OBJECT_B);
+
+        then(statistics).should().peek(ArithmeticOperation.ADD);
+    }
+
+
+    @Test
+    public void peeksStatisticsOnSubtracting() throws Exception {
+        calculator.subtract(OBJECT_A, OBJECT_B);
+
+        then(statistics).should().peek(ArithmeticOperation.SUBTRACT);
+    }
+
+
+    @Test
+    public void peeksStatisticsOnDividing() throws Exception {
+        calculator.divide(OBJECT_A, OBJECT_B);
+
+        then(statistics).should().peek(ArithmeticOperation.DIVIDE);
+    }
+
+
+    @Test
+    public void peeksStatisticsOnMultiplying() throws Exception {
+        calculator.multiply(OBJECT_A, OBJECT_B);
+
+        then(statistics).should().peek(ArithmeticOperation.MULTIPLY);
+    }
+}
